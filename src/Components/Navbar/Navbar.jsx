49bb358dd5c9ab1b054e6172965a1d34ec931589
Ahmed Mohamed <sabry9800@gmail.com>
2024-08-01 02:36:05 +0300

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-dom'
+export default function Navbar() {
+  return <>
+  
+  <nav>
+    <ul>
+      <li>sdsda</li>
+    </ul>
+  </nav>
+
+  
+  </>
+  
+}
